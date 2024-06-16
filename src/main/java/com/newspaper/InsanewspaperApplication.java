8999605ddbfc9295f5d1c7a74eb6a287a7327fb1
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-06-15 21:36:09 -0500

    first commit

---
+++ src/main/java/com/newspaper/InsanewspaperApplication.java
@@ -0,0 +1,13 @@
+package com.newspaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsanewspaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsanewspaperApplication.class, args);
+	}
+
+}
