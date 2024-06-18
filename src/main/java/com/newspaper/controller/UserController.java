a596ffc8d022fbda72e91c9533dfa63b4338961e
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-06-17 19:46:19 -0500

    first interface

--- src/main/java/com/newspaper/controller/UserController.java
+++ src/main/java/com/newspaper/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.newspaper.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/v1")
+    public String principalPage(Model model) {
+        return "principal";
+    }
+
 }
